2e73c05e86ad9f3f26d34ef9ff42a8e1f9016672
hiroki.konnai <felica.h.konnai@gmail.com>
2025-07-25 13:58:52 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
+import { Spanner } from '@google-cloud/spanner'
 
+export async function start () {
+  process.env.DEVMGR_FW_COMMON_PROJECT_NAME='macnica-mcp-devmgr-target'
+  process.env.DEVMGR_FW_DEVREPO_NAME='dev-instance'
+  process.env.DEVMGR_FW_DEVREPO_DB='devrepo'
 
+  const spanner = new Spanner({ projectId: process.env.DEVMGR_FW_COMMON_PROJECT_NAME })
 
-console.log(`text: ${process.env.SAMPLE_SECRET}`)
+  const instance = spanner.instance(process.env.DEVMGR_FW_DEVREPO_NAME)
+  const db = instance.database(process.env.DEVMGR_FW_DEVREPO_DB)
 
-process.exit(0)
+  const [rows] = await db.run({ sql: 'SELECT count(*) FROM device', params: {} })
+  console.log(rows[0])
+
+  process.exit(0)
+}
+
+start().catch((error) => {
+  console.error('Failed to Start:', error)
+  process.exit(1)
+})
